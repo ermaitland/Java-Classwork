d7d3f416a297b1723d2f0e9917aa04a7d626ee73
Eleanor Maitland <maitland.eleanor@gmail.com>
2023-05-10 15:58:58 +0100

    End of Day 2, week 1

---
+++ JavaBasics/FirstJavaDemoProject/src/FirstAssignement.java
@@ -0,0 +1,27 @@
+
+public class FirstAssignement {
+
+	public static void main(String[] args) {
+		
+		short value1=10;
+		float value2=56.9F;
+		boolean value3=false;
+		char value4='S';
+		String value5="Prisca";
+		boolean value6=true;
+		int value7=347890;
+		long value8=2147483650L;
+		
+		System.out.println(value1);
+		System.out.println(value2);
+		System.out.println(value3);
+		System.out.println(value4);
+		System.out.println(value5);
+		System.out.println(value6);
+		System.out.println(value7);
+		System.out.println(value8);		
+
+		
+	}
+
+}
---
+++ JavaBasics/FirstJavaDemoProject/src/MathAssignment.java
@@ -0,0 +1,18 @@
+
+public class MathAssignment {
+
+	public static void main(String[] args) {
+		int feet;
+		int inches;
+		
+		int heightInInches = 18;
+		
+		feet=heightInInches/12;
+		inches=heightInInches%12;
+		
+		System.out.println("You are: "+feet+" foot "+inches+" inches tall");
+		
+
+	}
+
+}
