3701ed4a566d43cd5b8c73fd1de6f53879342f32
Eleanor Maitland <maitland.eleanor@gmail.com>
2023-05-16 13:49:16 +0100

    jagged matrix

--- week-2-code-along/Day2Week2/src/JaggedMatrix.java
+++ week-2-code-along/Day2Week2/src/JaggedMatrix.java
@@ -1,8 +1,35 @@
+import java.util.Scanner;
 
 public class JaggedMatrix {
 
 	public static void main(String[] args) {
+		int arr[][];
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number of rows:");
+		int rows=sc.nextInt();
 		
+		arr=new int[rows][]; // memory area for rows
+		
+		for(int row=0;row<rows;row++) {
+			// enter number of columns
+			System.out.println("Enter number of cols for row "+(row+1));
+			int cols=sc.nextInt();
+			//assign memory for cols
+			arr[row]=new int[cols];
+			for (int col=0;col<cols;col++) {
+				// enter the values for the elements in that row
+				System.out.println("Wnter element at ["+row+"]["+col+"] location : ");
+				arr[row][col]=sc.nextInt();
+			}
+		}
+		
+		for(int row=0;row<rows;row++) {
+			//display - use the arr[row].length as cannot access the columns
+			for(int col=0;col<arr[row].length;col++) {
+				System.out.print(arr[row][col]+"\t");
+			}
+			System.out.println("");
+		}
 
 	}
 
