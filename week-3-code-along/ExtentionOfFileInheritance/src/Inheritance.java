8ff37109b390f8de9eeae2318a472114fc9d3326
Eleanor Maitland <maitland.eleanor@gmail.com>
2023-05-22 14:48:31 +0100

    Inheritance

---
+++ week-3-code-along/ExtentionOfFileInheritance/src/Inheritance.java
@@ -0,0 +1,7 @@
+import com.maitland.method.Calculator;
+
+public class Inheritance extends Calculator {
+// Can access the public methods in calculator
+	
+	//import the JAR file, then can change the superClass to the package you want to extend. It will automatically do the rest
+}
